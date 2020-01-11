31f76ae75c9aa312712b2068e2cc64deadb20f59
CQZ <752521865@qq.com>
2020-01-11 11:21:24 +0800

    HashMap&List&String

--- src/main/java/com/imomei/Test.java
+++ src/main/java/com/imomei/Test.java
@@ -1,8 +1,13 @@
 package com.imomei;
 
+import com.imomei.string.IMoMei_String;
+
 import java.util.*;
 
 public class Test {
+    private String a;
+    private String b;
+
     public static void main(String[] args) {
         Map map = new HashMap<String, String>();
         map.put("1", "11");
@@ -10,8 +15,13 @@ public class Test {
         map.put("3", "33");
         Collection values = map.values();
         Iterator iterator = values.iterator();
+        ArrayList<String> arrayList = new ArrayList<>();
+        Iterator<String> iterator1 = arrayList.iterator();
+
         if (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
     }
+
+
 }
